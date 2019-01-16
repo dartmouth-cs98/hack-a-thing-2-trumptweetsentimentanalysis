4d1f9c03698fee372d5647724186a6a60e1779bc
Azhar Hussain <azhar.m.hussain.19@dartmouth.edu>
2019-01-15 23:45:44 -0500

    added pulling historic S&P 500 prices

--- ToneAnalyzer.py
+++ ToneAnalyzer.py
@@ -3,8 +3,14 @@ from watson_developer_cloud import ToneAnalyzerV3
 
 import json
 import sys
-
+from dateutil import parser
+import datetime
+import requests
 import twitter
+
+# used python-twitter api to get tweets
+# used IBM Watson api to do sentiment analysis
+# used AlphaVantage api to get stock prices
 
 ACCESS_TOKEN_KEY = '334444072-Xj2lrc5Xh51bRi4xvadXwhdyzI7mAhCLyWJEzzYr'
 ACCESS_TOKEN_SECRET = 'wwSKRsN47dWjfFcQbkPTGp2lnljbNIhmMPaOEeWlf3ssp'
@@ -31,16 +37,23 @@ def get_tweets(api=None, screen_name=None):
 
     return timeline
 
+trump_tweets = set()
 
-if __name__ == "__main__":
-    api = twitter.Api(
-        CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET
-    )
-    screen_name = 'realDonaldTrump'
-    print(screen_name)
-    timeline = get_tweets(api=api, screen_name=screen_name)
+# api = twitter.Api(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+# screen_name = 'realDonaldTrump'
+# print(screen_name)
+# timeline = get_tweets(api=api, screen_name=screen_name)
+#
+# for tweet in timeline:
+#     print(tweet)
+#     print('\n')
 
-    # with open('examples/timeline.json', 'w+') as f:
-    for tweet in timeline:
-            print(tweet)
-            print('\n')
+dt = parser.parse('Sun Mar 11 13:41:04 +0000 2018')
+date = datetime.datetime.strptime(str(dt.day) + str(dt.month) + str(dt.year), '%d%m%Y').date()
+
+url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=SPX&outputsize=full&apikey=1TTCW4N7SLIUCIWS"
+
+stockResponse = requests.get(url)
+jData = json.loads(stockResponse.content)
+print(jData['Time Series (Daily)']['2017-06-23'])
+
